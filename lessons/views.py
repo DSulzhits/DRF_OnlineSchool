468b1194cfa0d4c2a7ec333c956feadac941960a
DSulzhits <dsulzhits@gmail.com>
2023-07-25 22:56:50 +0300

    add Lessons views + urls + serializers

--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, RetrieveAPIView, DestroyAPIView
 
-# Create your views here.
+from lessons.models import Lesson
+from lessons.serializers import LessonSerializer
+
+
+class LessonCreateAPIView(CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyAPIView(DestroyAPIView):
+    queryset = Lesson.objects.all()
