a4c0fdfd0ce6fddb329327d1233d5cc21836e5f7
DSulzhits <dsulzhits@gmail.com>
2023-07-25 21:55:37 +0300

    add model: Course

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import NULLABLE
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='курс')
+    preview = models.ImageField(upload_to='courses/', verbose_name='превью', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
