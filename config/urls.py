4906175168a0e9a8455b9cd7f0083ae7b3c953ef
DSulzhits <dsulzhits@gmail.com>
2023-07-26 18:41:00 +0300

    delete lessons urls from config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('courses.urls', namespace='courses')),
-                  path('lessons/', include('lessons.urls', namespace='lessons')),
                   # path('users/', include('users.urls', namespace='users')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
