82e7729b2d67597c6926e6731073cd7407fc55d2
DSulzhits <dsulzhits@gmail.com>
2023-07-26 18:26:20 +0300

    delete app users

--- lessons/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from users.models import NULLABLE
-
-
-class Lesson(models.Model):
-    title = models.CharField(max_length=150, verbose_name='название')
-    description = models.TextField(verbose_name='описание', **NULLABLE)
-    preview = models.ImageField(upload_to='lessons/', verbose_name='превью', **NULLABLE)
-    video_url = models.URLField(verbose_name='видео', **NULLABLE)
--- lessons/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-
-from lessons.models import Lesson
-
-
-class LessonSerializer(ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = '__all__'
