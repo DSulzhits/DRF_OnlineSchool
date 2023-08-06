e6e4e6438f51baba81744755d8670e45ee9dc853
DSulzhits <dsulzhits@gmail.com>
2023-08-06 20:06:03 +0300

    add django_filters in INSTALLED_APPS, add DEFAULT_FILTER_BACKENDS in REST_FRAMEWORK

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
+
     'users',
     'courses',
 ]
@@ -133,3 +135,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
