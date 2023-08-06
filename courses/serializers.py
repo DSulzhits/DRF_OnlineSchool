85a3f98c13c9aded3ea98b5e7bd7484670fc3759
DSulzhits <dsulzhits@gmail.com>
2023-08-06 16:58:40 +0300

    update CourseSerializer

--- courses/serializers.py
+++ courses/serializers.py
@@ -3,13 +3,15 @@ from rest_framework.serializers import ModelSerializer
 from courses.models import Course, Lesson
 
 
-class CourseSerializer(ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
+        fields = '__all__'
+
+
+class CourseSerializer(ModelSerializer):
+    lessons = LessonSerializer(many=True, read_only=True, source='lesson_set')
+
+    class Meta:
+        model = Course
         fields = '__all__'
