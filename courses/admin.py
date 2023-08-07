d257754823dd1280f62103ce3d9fcdddf55cfa2b
DSulzhits <dsulzhits@gmail.com>
2023-08-07 17:25:49 +0300

    register models in courses/admin.py

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from courses.models import Course, Lesson
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('course', 'title',)
