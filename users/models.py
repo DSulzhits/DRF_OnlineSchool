e482e1290a73b0f97f4abe36af6ce169f1106444
DSulzhits <dsulzhits@gmail.com>
2023-08-09 15:23:28 +0300

    create model UserRoles, update model User

--- users/models.py
+++ users/models.py
@@ -1,17 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 NULLABLE = {'blank': True, 'null': True}
+
+
+class UserRoles(models.TextChoices):
+    MEMBER = 'member', _('member')
+    MODERATOR = 'moderator', _('moderator')
 
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='Email')
-
-    phone = models.CharField(max_length=35, verbose_name='номер телефона', **NULLABLE)
-    city = models.CharField(max_length=150, verbose_name='город', **NULLABLE)
-    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
-    is_active = models.BooleanField(default=True, verbose_name='активный')
+    role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.MEMBER)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
