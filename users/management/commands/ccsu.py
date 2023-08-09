d9d0c8b48ce057e88d8c790c2d3404d633a8b912
DSulzhits <dsulzhits@gmail.com>
2023-08-09 16:32:58 +0300

    expand command

--- users/management/commands/ccsu.py
+++ users/management/commands/ccsu.py
@@ -9,14 +9,41 @@ load_dotenv()
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user = User.objects.create(
+        admin = User.objects.create(
             email='admin@sky.pro',
             first_name='Admin',
             last_name='Adminov',
+            role='moderator',
             is_staff=True,
             is_superuser=True,
             is_active=True
         )
 
-        user.set_password(os.getenv("SUPERUSER_PASSWORD"))
-        user.save()
+        admin.set_password(os.getenv("SUPERUSER_PASSWORD"))
+        admin.save()
+
+        moderator = User.objects.create(
+            email='moderator@sky.pro',
+            first_name='Moderator',
+            last_name='Moderatov',
+            role='moderator',
+            is_staff=True,
+            is_superuser=False,
+            is_active=True
+        )
+
+        moderator.set_password(os.getenv("SUPERUSER_PASSWORD"))
+        moderator.save()
+
+        member = User.objects.create(
+            email='member@sky.pro',
+            first_name='Member',
+            last_name='Memberov',
+            role='member',
+            is_staff=False,
+            is_superuser=False,
+            is_active=True
+        )
+
+        member.set_password(os.getenv("SUPERUSER_PASSWORD"))
+        member.save()
