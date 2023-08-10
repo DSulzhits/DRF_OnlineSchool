7eabc94807d7baa53cb6fd7730cc2a3b717567df
DSulzhits <dsulzhits@gmail.com>
2023-08-10 08:26:36 +0300

    update urls for new views

--- courses/urls.py
+++ courses/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from courses.apps import CoursesConfig
 from rest_framework.routers import DefaultRouter
 
-from courses.views import CourseViewSet, LessonListCreateAPIView, LessonRetrieveUpdateDestroyAPIView, \
-    PaymentListCreateAPIView
+from courses.views import CourseViewSet, PaymentListCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonCreateAPIView, LessonUpdateAPIView, LessonDeleteAPIView
 
 app_name = CoursesConfig.name
 
@@ -13,9 +13,11 @@ router.register(prefix=r'courses', viewset=CourseViewSet, basename='courses')
 
 urlpatterns = [
                   # lessons
-                  path('lessons/', LessonListCreateAPIView.as_view(), name='lessons_list_create'),
-                  path('lessons/<int:pk>/', LessonRetrieveUpdateDestroyAPIView.as_view(),
-                       name='lessons_get_update_delete'),
+                  path('lessons/', LessonListAPIView.as_view(), name='lessons_list'),
+                  path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_get'),
+                  path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+                  path('lesson/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+                  path('lesson/<int:pk>/delete/', LessonDeleteAPIView.as_view(), name='lesson_delete'),
 
                   # payments
                   path('payments/', PaymentListCreateAPIView.as_view(), name='payments_list_create'),
