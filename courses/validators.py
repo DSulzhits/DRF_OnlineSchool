59e68a5d065979f6b8273a5b8d7601c88d00a18b
DSulzhits <dsulzhits@gmail.com>
2023-08-19 11:15:01 +0300

    Add ScamValidator

--- courses/validators.py
+++ courses/validators.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 
 
-def validator_scam_links(value):
-    if value.get('url_video'):
-        if 'youtube.com' not in value.get('url_video'):
-            raise serializers.ValidationError('Видео можно предоставлять только с youtube.com')
+class ScamValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        if value.get('video_url'):
+            if 'youtube.com' not in value.get('video_url'):
+                raise serializers.ValidationError('Видео можно предоставлять только с youtube.com')
+
+# def validator_scam_links(value):
+#     if value.get('url_video'):
+#         if 'youtube.com' not in value.get('url_video'):
+#             raise serializers.ValidationError('Видео можно предоставлять только с youtube.com')
