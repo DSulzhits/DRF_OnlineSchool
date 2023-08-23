8394673227dd46bcc0e5db181f19191e7e7b27a1
DSulzhits <dsulzhits@gmail.com>
2023-08-23 14:27:19 +0300

    add new urls

--- courses/urls.py
+++ courses/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from courses.apps import CoursesConfig
 from rest_framework.routers import DefaultRouter
 
-from courses.views import CourseViewSet, PaymentListCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
-    LessonCreateAPIView, LessonUpdateAPIView, LessonDeleteAPIView
+from courses.views import CourseViewSet, PaymentListView, PaymentCreateAPIView, LessonListAPIView, \
+    LessonRetrieveAPIView, \
+    LessonCreateAPIView, LessonUpdateAPIView, LessonDeleteAPIView, PaymentGetView, SubscriptionCreateAPIView, \
+    SubscriptionUpdateView
 
 app_name = CoursesConfig.name
 
@@ -20,5 +22,11 @@ urlpatterns = [
                   path('lesson/<int:pk>/delete/', LessonDeleteAPIView.as_view(), name='lesson_delete'),
 
                   # payments
-                  path('payments/', PaymentListCreateAPIView.as_view(), name='payments_list_create'),
+                  path('payments/', PaymentListView.as_view(), name='payments_list'),
+                  path('payment/create/', PaymentCreateAPIView.as_view(), name='payment_create'),
+                  path('payment/<str:payment_id>/', PaymentGetView.as_view(), name='payment_get'),
+
+                  # subscription
+                  path('subscription/create/', SubscriptionCreateAPIView.as_view(), name='subscription_create'),
+                  path('subscription/<int:pk>/update/', SubscriptionUpdateView.as_view(), name='subscription_update'),
               ] + router.urls
