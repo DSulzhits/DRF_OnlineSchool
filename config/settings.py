d26622d1c8331e594d6ab7864b620ac636167308
DSulzhits <dsulzhits@gmail.com>
2023-08-24 18:01:47 +0300

    add: CELERY_BROKER_URL CELERY_RESULT_BACKEND

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework_simplejwt',
     'corsheaders',
+    'django_celery_beat',
 
     'users',
     'courses',
@@ -171,3 +172,6 @@ STRIPE_URL = os.getenv("STRIPE_URL")
 ALLOWED_DOMAINS = [
     'youtube.com'
 ]
+
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
